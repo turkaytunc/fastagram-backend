6691d9289a99e0319ffbc25f3545a7c35d0e8a75
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-03 15:53:20 +0300

    remove: console logs

--- src/middlewares/validate-user/validateUser.ts
+++ src/middlewares/validate-user/validateUser.ts
@@ -8,16 +8,11 @@ const secret = process.env.JWT_SECRET!;
 
 export const validateUser = async (req: any, res: any, next: any) => {
   try {
-    console.log(req.cookies);
     const { auth } = req.cookies;
 
-    console.log(auth);
-
     const token = jwt.verify(auth, secret) as any;
-
-    console.log(token);
-
     req.user = token.user;
+
     return next();
   } catch (error) {
     const err = new HttpError('You must provide valid auth token', 403);
