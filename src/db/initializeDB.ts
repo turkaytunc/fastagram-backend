a692762729dc6471f36d46a07c9d7483b227f48a
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-11 11:16:34 +0300

    add: comments, likes and photos tables

--- src/db/initializeDB.ts
+++ src/db/initializeDB.ts
@@ -3,11 +3,34 @@ import pool from './pool';
 export const initializeDB = async () => {
   await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
   await pool.query(`
-  CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    user_id uuid DEFAULT uuid_generate_v4(),
-    name VARCHAR(20) NOT NULL,
-    password VARCHAR(200) NOT NULL,
-    email VARCHAR(50) NOT NULL
-)`);
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      user_id uuid DEFAULT uuid_generate_v4(),
+      username VARCHAR(20) NOT NULL,
+      fullname VARCHAR(20),
+      password VARCHAR(200) NOT NULL,
+      email VARCHAR(50) NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS photos (
+      id SERIAL PRIMARY KEY,
+      user_id INT NOT NULL REFERENCES users(id),
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+      data TEXT NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS comments (
+      id SERIAL PRIMARY KEY,
+      photo_id INT NOT NULL REFERENCES photos(id),
+      user_id INT NOT NULL REFERENCES users(id),
+      content VARCHAR(150) NOT NULL,
+      created_at DATE DEFAULT CURRENT_DATE
+    );
+
+    CREATE TABLE IF NOT EXISTS likes (
+      id SERIAL PRIMARY KEY,
+      photo_id INT NOT NULL REFERENCES photos(id),
+      user_id INT NOT NULL REFERENCES users(id),
+    );
+    `);
 };
