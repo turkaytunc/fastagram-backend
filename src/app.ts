7d6e82c23270ee35daf11bde065143905cdd63d6
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-15 16:16:45 +0300

    remove: userId requirement from photo add endpoint

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.text());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(helmet());
 app.use(morgan('dev'));
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -5,6 +5,6 @@ import { profileControllers } from '../controllers';
 const profile = express.Router();
 
 profile.get('/:userId', validateUser, profileControllers.fetchPhotos);
-profile.post('/addphoto/:userId', validateUser, profileControllers.addPhoto);
+profile.post('/addphoto/', validateUser, profileControllers.addPhoto);
 
 export default profile;
