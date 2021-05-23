417f23e72e33273d13701e2e925a6f67427b2a80
Türkay Tunç <turkaytunc00@gmail.com>
2021-05-23 20:52:38 +0300

    add: Comment queries

--- src/controllers/dashboardControllers.ts
+++ src/controllers/dashboardControllers.ts
@@ -2,6 +2,7 @@ import { RequestHandler, Request } from 'express';
 import { HttpError } from '../utils';
 import { findUserByEmail } from '../db/User';
 import { getFeedPhotos } from '../db/Feed';
+import { getCommentsByPhotoId } from '../db/Comment';
 
 interface UserRequest extends Request {
   user?: { email: string };
@@ -33,6 +34,21 @@ export const getFeedItems: RequestHandler = async (req: Request, res, next) => {
     }
 
     throw new HttpError('User not found', 404);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getComments: RequestHandler = async (req: Request, res, next) => {
+  try {
+    // eslint-disable-next-line camelcase
+    const { photo_id } = req.body;
+    const user = await getCommentsByPhotoId(photo_id);
+    if (user.rows[0]) {
+      return res.json({ comments: user.rows });
+    }
+
+    throw new HttpError('No comments found', 404);
   } catch (error) {
     return next(error);
   }
