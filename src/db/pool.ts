828b8ac74d53512d27cc86a886a242ddeac17258
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-05 11:18:59 +0300

    update: db url for deployment

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -5,17 +5,17 @@ dotenv.config();
 
 const { POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB, POSTGRES_PORT } = process.env;
 
-const pool = new pg.Pool({
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  port: (POSTGRES_PORT as unknown) as number,
-  database: POSTGRES_DB,
-  host: POSTGRES_HOST,
-});
-
 // const pool = new pg.Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: { rejectUnauthorized: false },
+//   user: POSTGRES_USER,
+//   password: POSTGRES_PASSWORD,
+//   port: (POSTGRES_PORT as unknown) as number,
+//   database: POSTGRES_DB,
+//   host: POSTGRES_HOST,
 // });
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
